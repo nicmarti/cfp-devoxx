3aba2255e107655c9fe42b705898df8c2e3053d8
Nicolas Martignole <nicolas@martignole.net>
2013-11-11 11:33:44 +0100

    Created Review object

---
+++ app/models/Review.scala
@@ -0,0 +1,44 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Association du Paris Java User Group.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package models
+
+import library.Redis
+
+/**
+ * When a CFP admin checks or perform a review on a talk, we store this event.
+ *
+ * Author: nicolas martignole
+ * Created: 11/11/2013 10:21
+ */
+case class Review(reviewer:String, proposalId:String)
+
+object Review{
+
+  def allProposalsNotReviewed(reviewer:String):List[Proposal]=Redis.pool.withClient{
+    client=>
+      Nil
+
+  }
+
+}
