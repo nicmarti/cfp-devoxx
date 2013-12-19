598e670999cc260ec7ccef4b8ae3b69abc54a654
Nicolas Martignole <nicolas@martignole.net>
2013-12-19 10:18:19 +0100

    code reformat

--- app/models/Review.scala
+++ app/models/Review.scala
@@ -206,19 +206,18 @@ object Review {
   }
 
   // How many talks submitted for Java? for Web?
-  def totalSubmittedByTrack() :List[(Track, Int)] = Redis.pool.withClient {
-      implicit client =>
+  def totalSubmittedByTrack(): List[(Track, Int)] = Redis.pool.withClient {
+    implicit client =>
 
-        val toRetn= for(proposalId<-client.smembers("Proposals:ByState:"+ProposalState.SUBMITTED.code).toList;
-                        track<-Proposal.findProposalTrack(proposalId)
-                        ) yield (track,1)
+      val toRetn = for (proposalId <- client.smembers("Proposals:ByState:" + ProposalState.SUBMITTED.code).toList;
+                        track <- Proposal.findProposalTrack(proposalId)
+      ) yield (track, 1)
 
-      toRetn.groupBy(_._1).map{case(category, listOfCategoryAndTotal)=>
-        (category,listOfCategoryAndTotal.map(_._2).sum)
+      toRetn.groupBy(_._1).map {
+        case (category, listOfCategoryAndTotal) =>
+          (category, listOfCategoryAndTotal.map(_._2).sum)
       }.toList
-
-
-    }
+  }
 
 
 }
