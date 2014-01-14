d43ced6259f604bd8cc0496d68b3de6afe87d877
Nicolas Martignole <nicolas@martignole.net>
2014-01-14 15:46:11 +0100

    Added Play Cache dependency

--- project/Build.scala
+++ project/Build.scala
@@ -12,13 +12,13 @@ object ApplicationBuild extends Build {
   val appName = "cfp-devoxxfr"
   val appVersion = "1.3-SNAPSHOT"
 
-
   val appDependencies = Seq(
     "redis.clients" % "jedis" % "2.1.0"
     , "com.typesafe" %% "play-plugins-mailer" % "2.1.0"
     , "org.apache.commons" % "commons-lang3" % "3.1"
     , "commons-io" % "commons-io" % "2.4"
     , "commons-codec" % "commons-codec" % "1.7" // for new Base64 that has support for String
+    , "com.typesafe.play" %% "play-cache" % "2.2.0"
   )
 
   libraryDependencies ++= Seq(
