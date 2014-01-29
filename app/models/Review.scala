f043a54379a518c2ff0fda070e2bc1a457c75b61
Nicolas Martignole <nicolas@martignole.net>
2014-01-29 10:35:55 +0100

    Bug fix

--- app/models/Review.scala
+++ app/models/Review.scala
@@ -247,7 +247,7 @@ object Review {
           (proposalId,
             (scores.toDouble, allVoters.get(proposalKey).map(_.toInt).getOrElse(0),
               allAbstentions.get(proposalKey).map(_.toInt).getOrElse(0),
-              allAverages.get(proposalKey).map(d=>BigDecimal(d.toDouble).setScale(3,RoundingMode.HALF_EVEN).toDouble).getOrElse(0.toDouble),
+              allAverages.get(proposalKey).filterNot(_=="nan").filterNot(_=="_nan").map(d=>BigDecimal(d.toDouble).setScale(3,RoundingMode.HALF_EVEN).toDouble).getOrElse(0.toDouble),
               allStandardDev.get(proposalKey).filterNot(_=="nan").filterNot(_=="_nan").map{
                 d=>
                   BigDecimal(d.toDouble).setScale(3,RoundingMode.HALF_EVEN).toDouble
