13f420a52c6d37f34b24e3a813aec7e537b2371b
Nicolas Martignole <nicolas@martignole.net>
2014-02-26 14:08:42 +0100

    AngularJS : directives

--- public/angular/js/app.js
+++ public/angular/js/app.js
@@ -17,51 +17,26 @@ cfpApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
 
-cfpApp.directive("drag", ["$rootScope", function($rootScope) {
-
-  function dragStart(evt, element, dragStyle) {
-    element.addClass(dragStyle);
-    evt.dataTransfer.setData("id", evt.target.id);
-    evt.dataTransfer.effectAllowed = 'move';
-  };
-
-  function dragEnd(evt, element, dragStyle) {
-    element.removeClass(dragStyle);
-  };
-
-  return {
-    restrict: 'A',
-    link: function(scope, element, attrs)  {
-      attrs.$set('draggable', 'true');
-      scope.dragData = scope[attrs["drag"]];
-      scope.dragStyle = attrs["dragstyle"];
-      element.bind('dragstart', function(evt) {
-        $rootScope.draggedElement = scope.dragData;
-        dragStart(evt, element, scope.dragStyle);
-      });
-      element.bind('dragend', function(evt) {
-        dragEnd(evt, element, scope.dragStyle);
-      });
-    }
-  }
-}]);
 
 cfpApp.directive("drop", ['$rootScope', function($rootScope) {
 
   function dragEnter(evt, element, dropStyle) {
     evt.preventDefault();
     element.addClass(dropStyle);
-  };
+  }
+
   function dragLeave(evt, element, dropStyle) {
     element.removeClass(dropStyle);
-  };
+  }
+
   function dragOver(evt) {
     evt.preventDefault();
-  };
+  }
+
   function drop(evt, element, dropStyle) {
     evt.preventDefault();
     element.removeClass(dropStyle);
-  };
+  }
 
   return {
     restrict: 'A',
