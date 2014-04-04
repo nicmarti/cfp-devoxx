fe5b207ad4a3ab82c0031fafb962ea56b260a342
Nicolas Martignole <nicolas@martignole.net>
2014-04-04 21:48:40 +0200

    Comet version

--- app/controllers/Tweetwall.scala
+++ app/controllers/Tweetwall.scala
@@ -49,7 +49,7 @@ object Tweetwall extends Controller {
 
   val cfg = Play.application.configuration
 
- val KEY = ConsumerKey(cfg.getString("twitter.consumerKey"), cfg.getString("twitter.consumerSecret"))
+  val KEY = ConsumerKey(cfg.getString("twitter.consumerKey"), cfg.getString("twitter.consumerSecret"))
 
 
   val TWITTER = OAuth(ServiceInfo(
@@ -68,15 +68,15 @@ object Tweetwall extends Controller {
       }
   }
 
-
   def outputRequest() = Action {
     implicit request =>
 
-      println("URI "+request.uri)
-      request.headers.toSimpleMap.foreach{token=>
-        println("Header "+ token._1+"="+token._2)
+      println("URI " + request.uri)
+      request.headers.toSimpleMap.foreach {
+        token =>
+          println("Header " + token._1 + "=" + token._2)
       }
-      println("Body "+request.body.asFormUrlEncoded)
+      println("Body " + request.body.asFormUrlEncoded)
       println("---")
       Ok("Done")
   }
@@ -104,6 +104,22 @@ object Tweetwall extends Controller {
           })
   }
 
+  def watchTweets(keywords: String) = Action {
+    implicit request =>
+
+      val (tweetsOut, tweetChanel) = Concurrent.broadcast[JsValue]
+      WS.url(s"https://stream.twitter.com/1.1/statuses/filter.json?track=" + URLEncoder.encode(keywords, "UTF-8"))
+        .sign(OAuthCalculator(KEY, sessionTokenPair.get))
+        .postAndRetrieveStream("")(headers => Iteratee.foreach[Array[Byte]] {
+        ba =>
+          val msg = new String(ba, "UTF-8")
+          val tweet = Json.parse(msg)
+          tweetChanel.push(tweet)
+      }).flatMap(_.run)
+
+      Ok.chunked(tweetsOut &> Comet(callback = "parent.cometMessage"))
+  }
+
   def sessionTokenPair(implicit request: RequestHeader): Option[RequestToken] = {
     for {
       token <- request.session.get("token")
@@ -113,125 +129,5 @@ object Tweetwall extends Controller {
     }
   }
 
-  implicit val writeTweetAsJson = Json.writes[Tweet]
-
-  def search(query: String) = Action {
-    implicit request =>
-      val asJson: Enumeratee[Tweet, JsValue] = Enumeratee.map {
-        case t => Json.toJson(t)
-      }
-      val tokens = sessionTokenPair(request).get
-
-      // Serve a 200 OK text/event-stream
-      Ok.feed(Tweet.search(query, tokens) &> asJson &> EventSource()
-      ).as("text/event-stream")
-
-  }
-
-  case class Tweet(id: String, text: String, image: String)
-
-  object Tweet {
-
-    // Reads the twitter search API response, as a Seq[Tweet]
-    implicit val readTweet: Reads[Seq[Tweet]] =
-
-    // Start with 'results' property
-      (__ \ "results").read(
-        // It contains an array, so for each item
-        seq(
-          // Read the tweet id encoded as String
-          (__ \ "id_str").read[String] and
-
-            // Read the tweet text content
-            (__ \ "text").read[String] and
-
-            // If there is an {entities: {media: [] ... property
-            (__ \ "entities" \ "media").readNullable(
-              // It contains an array, so for each item
-              seq(
-                // Read the image URL
-                (__ \ "media_url").read[String]
-              )
-
-              // Let's transform the Option[Seq[String]] to an simple Option[String]
-              // since we care only about the first image URL if there is any.
-            ).map(_.flatMap(_.headOption))
-
-            // Transform all this to a (String, String, Option[String]) tuple
-            tupled
-        )
-          .map(
-            // Keep only the tuple containing an Image (third part of the tuple is Some)
-            // and transform them to Tweet instances.
-            _.collect {
-              case (id, text, Some(image)) => Tweet(id, text, image)
-            }
-          )
-      )
-
-    def fetchPage(query: String, page: Int, secureToken: RequestToken): Future[Seq[Tweet]] = {
-      println("Fetch page...")
-      // Fetch the twitter search API with the corresponding parameters (see the Twitter API documentation)
-
-
-      WS.url("https://stream.twitter.com/1.1/statuses/filter.json?track=" + URLEncoder.encode(query, "UTF-8"))
-        .sign(OAuthCalculator(KEY, secureToken))
-        .get().map{
-        result=>
-          println("Result "+result.body)
-          sys.error("nothing")
-      }
-
-//     WS.url("https://stream.twitter.com/1.1/statuses/filter.json")
-//WS.url("http://localhost:7777/debug")
-//        .sign(OAuthCalculator(KEY, secureToken))
-//        .withHeaders(("Connection"->"close"))
-//        .post(Map("track"->Seq("tennis")))
-//
-//        .map(r => r.status match {
-//
-//        // We got a 200 OK response, try to convert the JSON body to a Seq[Tweet]
-//        // by using the previously defined implicit Reads[Seq[Tweet]]
-//        case 200 => {
-//          play.Logger.of("application.Tweetwall").info("Received " + r.body)
-//          r.json.asOpt[Seq[Tweet]].getOrElse(Nil)
-//        }
-//
-//        // Really? There is nothing todo for us
-//        case x => {
-//          play.Logger.of("application.Tweetwall").error("2 Unable to fetch Twitter Stream api " + r.statusText)
-//          play.Logger.of("application.Tweetwall").error(r.getAHCResponse.getHeaders().toString)
-//          sys.error("Fatal")
-//
-//        }
-//      })
-
-    }
-
-
-    /**
-     * Create a stream of Tweet object from a Twitter query (such as #devoxx)
-     */
-    def search(query: String, secureToken: RequestToken): Enumerator[Tweet] = {
-
-      // Flatenize an Enumerator[Seq[Tweet]] as n Enumerator[Tweet]
-      val flatenize = Enumeratee.mapConcat[Seq[Tweet]](identity)
-
-      // Schedule
-      val schedule = Enumeratee.mapM[Tweet](t => play.api.libs.concurrent.Promise.timeout(t, 1000))
-
-      // Create a stream of tweets from multiple twitter API calls
-      val tweets = Enumerator.unfoldM(1) {
-        case page => fetchPage(query, page, secureToken).map {
-          tweets =>
-            Option(tweets).filterNot(_.isEmpty).map(tweets => (page + 1, tweets))
-        }
-      }
-
-      // Compose the final stream
-      tweets &> flatenize &> schedule
-    }
-
-  }
 
 }
