e476974ae3bb580be26e9fa276108fa0f7d29ec9
Nicolas Martignole <nicolas@martignole.net>
2014-04-05 19:05:58 +0200

    Javascript timer

--- public/js/tweetwall.js
+++ public/js/tweetwall.js
@@ -33,7 +33,7 @@ $(function() {
             var tweet = JSON.parse(e.originalEvent.data);
             if (tweet && tweet.user) {
                   $('#list').prepend($("#tweetTemplate").render(tweet));
-                  if($('#list li').length>10) {
+                  if($('#list li').length>7) {
                       $('#list').find('li:last-child').remove();
                   }
 
@@ -56,6 +56,29 @@ $(function() {
             search($(this).val());
         }
     });
+
+    function checkTime(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        // add a zero in front of numbers<10
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('wallTime').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+
+    startTime();
 
 
 });
