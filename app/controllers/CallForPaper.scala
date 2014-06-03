46957aed8ba240b1a1e2892776bfcfec73500961
Nicolas Martignole <nicolas@martignole.net>
2014-06-03 15:36:26 +0200

    Closing issue #134Markdown issues on "Private Message"

--- app/controllers/CallForPaper.scala
+++ app/controllers/CallForPaper.scala
@@ -135,8 +135,9 @@ object CallForPaper extends SecureCFPController {
       Proposal.proposalForm.bindFromRequest.fold(
         hasErrors => BadRequest(views.html.CallForPaper.newProposal(hasErrors)).flashing("error" -> "invalid.form"),
         validProposal => {
-          val html = validProposal.summaryAsHtml // markdown to HTML
-          Ok(views.html.CallForPaper.previewProposal(html, Proposal.proposalForm.fill(validProposal), request.webuser.uuid))
+          val summary = validProposal.summaryAsHtml // markdown to HTML
+          val privateMessage = validProposal.privateMessageAsHtml // markdown to HTML
+          Ok(views.html.CallForPaper.previewProposal(summary, privateMessage, Proposal.proposalForm.fill(validProposal), request.webuser.uuid))
         }
       )
   }
