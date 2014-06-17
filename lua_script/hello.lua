b96950a678cdb04f912f90bffad778c4543c3929
Nicolas Martignole <nicolas@martignole.net>
2014-06-17 13:33:05 +0200

    bug(lua): fixed bug in stats with Reviewers

--- lua_script/hello.lua
+++ lua_script/hello.lua
@@ -1,15 +1,22 @@
 local proposals = redis.call("KEYS", "Proposals:Votes:*")
-for i = 1, #proposals do
-	redis.log(redis.LOG_DEBUG, "proposal i=" .. proposals[i])	
+redis.call("DEL", "Computed:Reviewer:Total")
 
-	local uuidAndScores = redis.call("ZRANGE", proposals[i], 0, 10, "WITHSCORES")
-	redis.call("HSET", "Computed:Scores", proposals[i], 0)
- 	redis.call("HSET", "Computed:Voters", proposals[i], 0)
-	
-	for j=1,#uuidAndScores,2 do
-  		redis.log(redis.LOG_DEBUG, "uuid:"..  uuidAndScores[j] .. " score:" .. uuidAndScores[j + 1])
-		redis.call("HINCRBY", "Computed:Scores", proposals[i], uuidAndScores[j + 1])
-		redis.call("HINCRBY", "Computed:Voters", proposals[i], 1)
-	end
+for i = 1, #proposals do
+  redis.log(redis.LOG_DEBUG, "==> " .. proposals[i])
+
+  local uuidAndScores = redis.call("ZRANGEBYSCORE", proposals[i], 0, 11, "WITHSCORES")
+  redis.call("HSET", "Computed:Scores", proposals[i], 0)
+  redis.call("HSET", "Computed:Voters", proposals[i], 0)
+  redis.call("HSET", "Computed:Average", proposals[i], 0)
+  redis.call("HSET", "Computed:Median", proposals[i], 0)
+  redis.call("HDEL", "Computed:Votes:ScoreAndCount", proposals[i])
+  redis.call("HDEL", "Computed:VotersAbstention", proposals[i])
+  redis.call("HDEL", "Computed:StandardDeviation" , proposals[i])
+
+  for j=1,#uuidAndScores,2 do
+    redis.log(redis.LOG_DEBUG, "uuid:" ..  uuidAndScores[j] .. " score:" .. uuidAndScores[j + 1])
+    redis.call("HINCRBY", "Computed:Scores", proposals[i], uuidAndScores[j + 1])
+    redis.call("HINCRBY", "Computed:Voters", proposals[i], 1)
+    redis.call("HINCRBY", "Computed:Reviewer:Total", uuidAndScores[j], uuidAndScores[j + 1])
+  end
 end
-return #proposals
