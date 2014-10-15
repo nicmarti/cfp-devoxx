69993869744c41497c82c5504c856bdec8852bf9
Nicolas Martignole <nicolas@martignole.net>
2014-10-16 00:43:30 +0200

    Fix putain de parenthese de merde

--- app/library/search/ElasticSearchActor.scala
+++ app/library/search/ElasticSearchActor.scala
@@ -264,7 +264,7 @@ class IndexMaster extends ESActor {
           hit: HitView =>
             sb.append("{\"index\":{\"_index\":\"hitviews\", \"_type\":\"hitview\",\"_id\":\"" + hit.hashCode().toString + "\", \"_timestamp\":{\"enabled\":true}}}")
             sb.append("\n")
-            val date = new DateTime(hit.date * 1000).toDateTime(DateTimeZone.forID("Europe/Brussels")).toString
+            val date = new DateTime(hit.date * 1000).toDateTime(DateTimeZone.forID("Europe/Brussels")).toString()
             sb.append("{\"@tags\":\"").append(hit.url).append("\",\"@messages\":\"")
             //.append(hit.objRef).append(" ")
             sb.append(hit.objName.replaceAll("[-,\\s+]", "_")).append("\",\"@timestamp\":\"").append(date).append("\"}")
