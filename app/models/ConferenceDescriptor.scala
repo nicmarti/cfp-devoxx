dc9194dc702bd733f85cbf1f54a331587f873cb7
Nicolas Martignole <nicolas@martignole.net>
2015-04-05 08:59:50 +0200

    feat(agenda): Fix for Castcodeurs

--- app/models/ConferenceDescriptor.scala
+++ app/models/ConferenceDescriptor.scala
@@ -558,7 +558,7 @@ object ConferenceDescriptor {
       val conferenceFridaySlot6 = List(
           SlotBuilder(ConferenceProposalTypes.CONF.id, "friday",
             new DateTime("2015-04-10T17:40:00.000+02:00").toDateTime(DateTimeZone.forID("Europe/Paris")),
-            new DateTime("2015-04-10T18:30:00.000+02:00").toDateTime(DateTimeZone.forID("Europe/Paris")), ConferenceRooms.AMPHI_BLEU)
+            new DateTime("2015-04-10T18:30:00.000+02:00").toDateTime(DateTimeZone.forID("Europe/Paris")), ConferenceRooms.MAILLOT)
       )
 
       conferenceFridaySlot1 ++ conferenceFridaySlot2 ++ conferenceFridaySlot3 ++ conferenceFridaySlot4 ++ conferenceFridaySlot5 ++ conferenceFridaySlot6
