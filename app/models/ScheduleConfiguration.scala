7722b1ff88c029ba28c04a8c489bb0befde734ee
Nicolas Martignole <nicolas@martignole.net>
2015-04-08 16:58:06 +0200

    cfp

--- app/models/ScheduleConfiguration.scala
+++ app/models/ScheduleConfiguration.scala
@@ -185,10 +185,10 @@ object ScheduleConfiguration {
   }
 
 
-  def loadNextTalks() = {
+  def  loadNextTalks() = {
     val allAgendas = ScheduleConfiguration.loadAllConfigurations()
     val slots = allAgendas.map(_.slots).flatten
-    Option(slots.filter(_.from.isAfter(new DateTime().toDateTime(DateTimeZone.forID("Europe/Brussels")))).sortBy(_.from.toDate.getTime).take(10))
+    Option(slots.filter(_.from.isAfter(new DateTime().toDateTime(DateTimeZone.forID("Europe/Paris")))).sortBy(_.from.toDate.getTime).take(10))
   }
 
   def loadRandomTalks() = {
