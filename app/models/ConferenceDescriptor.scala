e6daf1e20f0b4e2489430e74934427dace2c85db
Stephan Janssen <sja@bejug.org>
2016-09-05 16:07:51 +0200

    Added Room 10 to Friday

--- app/models/ConferenceDescriptor.scala
+++ app/models/ConferenceDescriptor.scala
@@ -246,7 +246,7 @@ object ConferenceDescriptor {
     val eveningKeynoteRoom = List(ROOM5)
 
     val allRoomsConf = List(ROOM8, ROOM5, ROOM9, ROOM6, ROOM7, ROOM4, ROOM3, ROOM10)
-    val fridayRoomsConf = List(ROOM4, ROOM5, ROOM8, ROOM9)
+    val fridayRoomsConf = List(ROOM4, ROOM5, ROOM8, ROOM9, ROOM10)
 
     val allRoomsQuick = List(ROOM8, ROOM5, ROOM9, ROOM6, ROOM7, ROOM4, ROOM3)
 
@@ -702,7 +702,7 @@ object ConferenceDescriptor {
       toReturn
 
     }
-    // ROOM4, ROOM5, ROOM8, ROOM9
+    // ROOM4, ROOM5, ROOM8, ROOM9, ROOM10
     val conferenceSlotsFriday: List[Slot] = {
 
       val conferenceFridaySlot1 = ConferenceRooms.fridayRoomsConf.map {
