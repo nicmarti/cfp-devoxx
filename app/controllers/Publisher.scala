320660001ecf587f149dca8cc4bafa4adf037fd1
Stephan Janssen <sja@bejug.org>
2017-01-30 19:46:15 +0100

    - Now show tags on public proposals - List talks by tags view added

--- app/controllers/Publisher.scala
+++ app/controllers/Publisher.scala
@@ -127,6 +127,17 @@ object Publisher extends Controller {
       Ok(views.html.Publisher.showAllTalksByProposalType(proposals))
   }
 
+  def showByTag(tagId: String) = Action {
+    implicit request =>
+      val tag = Tag.findById(tagId)
+      if (tag.isDefined) {
+        val acceptedProposalsByTag = Tags.allProposalsByTagId(tagId).filter(entry => entry._2.state == ProposalState.ACCEPTED)
+        Ok(views.html.Publisher.showByTag(tag.get.value, acceptedProposalsByTag.values))
+      } else {
+        BadRequest("Tag not found")
+      }
+  }
+
   private val monday: String = "monday"
   private val tuesday: String = "tuesday"
   private val wednesday: String = "wednesday"
