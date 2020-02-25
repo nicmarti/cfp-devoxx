fd162be5000099e21a2b3d42d3a358887f4dbab1
fcamblor <fcamblor@gmail.com>
2020-02-25 23:31:45 +0100

    updated end time for thursday's labs from 18:30 to 18:15.
    Hardcoded slot's id to keep the old one, in order to not clutter the whole UI (both in angular slot config and on published schedule on the website)
    This hardcoding will have to be removed for DevoxxFr 2021

--- app/models/ConferenceDescriptor.scala
+++ app/models/ConferenceDescriptor.scala
@@ -398,9 +398,17 @@ object ConferenceDescriptor {
       }
       val labsThursdayAfternoon = ConferenceRooms.allRoomsLabThursday.map {
         r2 =>
-          SlotBuilder(ConferenceProposalTypes.LAB.id, "thursday",
+          // Temporary workaround in order to not change slot id in Prod Schedule
+          // FIXME: remove this and replace it with commented SlotBuilder() call in 2021
+          Slot(
+            s"${ConferenceProposalTypes.LAB.id}_${r2.id}_thursday_16_15h30_18h30", ConferenceProposalTypes.LAB.id, "thursday",
             new DateTime(s"${secondDay}T15:30:00.000+02:00").toDateTime(confTimezone),
-            new DateTime(s"${secondDay}T18:30:00.000+02:00").toDateTime(confTimezone), r2)
+            new DateTime(s"${secondDay}T18:15:00.000+02:00").toDateTime(confTimezone),
+            r2.id, None, None, None
+          )
+//          SlotBuilder(ConferenceProposalTypes.LAB.id, "thursday",
+//            new DateTime(s"${secondDay}T15:30:00.000+02:00").toDateTime(confTimezone),
+//            new DateTime(s"${secondDay}T18:15:00.000+02:00").toDateTime(confTimezone), r2)
       }
       labsThursdayMorning ++ labsThursdayAfternoon
     }
